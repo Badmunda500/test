709864a993f2682e1140639c0b66f83b081e0f87
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-06-07 12:29:37 +0530

    Update session.py

--- TelethonPbx/clients/session.py
+++ TelethonPbx/clients/session.py
@@ -1,95 +1,18 @@
-import sys
-
 from telethon import TelegramClient
-from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
+from TelethonPbx.config import API_ID, API_HASH, BOT_TOKEN
+from TelethonPbx.database import get_all_sessions
 
-from PbxConfig import Config
+clients = []  # All user clients
 
-
-def validate_session(session):
-    if "==Bad" and "bot==" in session.lower():
-        new_session = session[6:-5]
-        return str(new_session)
-    else:
-        print(f"PBXBOT SESSION - Wrong session string!")
-        sys.exit()
-
-if Config.PBXBOT_SESSION:
-    session = StringSession(str(Config.PBXBOT_SESSION))
-else:
-    session = "Pbxbot"
-
-
-try:
-    Pbx = TelegramClient(
-        session=session,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-except Exception as e:
-    print(f"PBXBOT_SESSION - {e}")
-    sys.exit()
-
-
-if Config.SESSION_2:
-    session2 = StringSession(str(Config.SESSION_2))
-    H2 = TelegramClient(
-        session=session2,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-else:
-    H2 = None
-
-
-if Config.SESSION_3:
-    session3 = StringSession(str(Config.SESSION_3))
-    H3 = TelegramClient(
-        session=session3,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-else:
-    H3 = None
-
-
-if Config.SESSION_4:
-    session4 = StringSession(str(Config.SESSION_4))
-    H4 = TelegramClient(
-        session=session4,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-else:
-    H4 = None
-
-
-if Config.SESSION_5:
-    session5 = StringSession(str(Config.SESSION_5))
-    H5 = TelegramClient(
-        session=session5,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-else:
-    H5 = None
-
+async def load_all_user_clients():
+    global clients
+    clients = []
+    all_sessions = await get_all_sessions()
+    for sess in all_sessions:
+        client = TelegramClient(StringSession(sess["session"]), API_ID, API_HASH)
+        await client.start()
+        clients.append(client)
 
 PbxBot = TelegramClient(
     session="Bad-TBot",
