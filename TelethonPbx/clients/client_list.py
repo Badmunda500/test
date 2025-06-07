8a07b4c150605717e7e1de58784dc3d77ccf8793
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-06-07 12:33:10 +0530

    Update client_list.py

--- TelethonPbx/clients/client_list.py
+++ TelethonPbx/clients/client_list.py
@@ -1,8 +1,7 @@
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.utils import get_peer_id
-from TelethonPbx.clients.session import H2, H3, H4, H5, Pbx
+from TelethonPbx.clients.session import clients, Pbx
 from TelethonPbx.DB.gvar_sql import gvarstat
-
 
 async def clients_list():
     user_ids = []
@@ -13,37 +12,24 @@ async def clients_list():
             user_ids.append(c)
     main_id = await Pbx.get_me()
     user_ids.append(main_id.id)
-
-    try:
-        if H2 is not None:
-            id2 = await H2.get_me()
-            user_ids.append(id2.id)
-    except:
-        pass
-
-    try:
-        if H3 is not None:
-            id3 = await H3.get_me()
-            user_ids.append(id3.id)
-    except:
-        pass
-
-    try:
-        if H4 is not None:
-            id4 = await H4.get_me()
-            user_ids.append(id4.id)
-    except:
-        pass
-
-    try:
-        if H5 is not None:
-            id5 = await H5.get_me()
-            user_ids.append(id5.id)
-    except:
-        pass
-
+    for client in clients:
+        try:
+            me = await client.get_me()
+            if me.id not in user_ids:
+                user_ids.append(me.id)
+        except Exception:
+            pass
     return user_ids
 
+async def get_client_by_userid(user_id):
+    for client in clients:
+        try:
+            me = await client.get_me()
+            if me.id == user_id:
+                return client
+        except Exception:
+            pass
+    return None
 
 async def client_id(event, botid=None, is_html=None):
     if botid is not None:
@@ -62,7 +48,6 @@ async def client_id(event, botid=None, is_html=None):
         _html = f"<a href='tg://user?id={II_BAD_BBY_II}'>{PBX_USER}</a>"
         Pbx_mention = _html if is_html else _mention
     return II_BAD_BBY_II, PBX_USER, Pbx_mention
-
 
 async def get_user_id(event, ids):
     if str(ids).isdigit():
