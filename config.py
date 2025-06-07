4c7ba2b088c6d52613a809ce8c025e481d64d075
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-06-07 12:46:55 +0530

    Update and rename vps_config.py to config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ class Development(Config):
     
     APP_ID = 27383453 # 666666 is a placeholder. Fill your 6 digit api id
     API_HASH = "4c246fb0c649477cc2e79b6a178ddfaa"  # replace this with your api hash
-    BOT_TOKEN = "your value"  # Create a bot from @BotFather and paste the token here
+    BOT_TOKEN = "7572603594:AAEE5WKw34dyxnX4eq6VRyIzznmpACkdFVE"  # Create a bot from @BotFather and paste the token here
     BOT_LIBRARY = "telethon"  # fill 'pyrogram' if you want pyrogram version of Pbxbot else leave it as it is.
     DATABASE_URL = "your value"  # A postgresql database url from elephantsql ( https://www.elephantsql.com/ )
     PBXBOT_SESSION = "Your value"  # telethon or pyrogram string according to BOT_LIBRARY
     HANDLER = "."  # Custom Command Handler
-    LOGGER_ID = -100  # Fill Your Group logger id -100
+    LOGGER_ID = -1002093247039  # Fill Your Group logger id -100
     SUDO_HANDLER = "."  # Custom Command Handler for sudo users.
 
 
